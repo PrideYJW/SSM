8fc571b37b9c087ada16d16a69a561a3dfe543e7
PrideYJW <18374532149@163.com>
2017-12-27 16:09:16 +0800

    增加了接口的方法

---
+++ src/main/java/com/service/UserService.java
@@ -0,0 +1,18 @@
+package com.service;
+
+import com.entity.UserEntity;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/12/16.
+ */
+
+public interface UserService {
+
+    public List<UserEntity> query4List(Map<String, String> map);
+
+
+    public int init();
+}
---
+++ src/main/java/com/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.service.impl;
+
+import com.dao.UserDao;
+import com.entity.UserEntity;
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/12/16.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserDao userDao;
+
+    public List<UserEntity> query4List(Map<String, String> map) {
+
+        return userDao.query4List(map);
+    }
+
+    public int init() {
+
+        return 0;
+    }
+
+
+}
